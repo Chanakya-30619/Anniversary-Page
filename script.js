3d887f6f6f43b075074eec8ac093d0990f225a1e
Chanakya-30619 <166978730+Chanakya-30619@users.noreply.github.com>
2024-06-14 14:27:11 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("beautyButton");
+var btn = document.getElementById("Button_1");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
